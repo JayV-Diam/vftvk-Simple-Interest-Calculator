d29cc5a53d6ca800f94b16a4e4908252eedd901f
JV Diamante <jay.vdiamante@gmail.com>
2021-04-07 11:27:25 +0000

    changes made on 04/07/2021

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var pincipal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var year = document.getElementById("years").value;
+    var interest = principal*years*rate / 100;
     
+    function SliderValue {
+       document.getElementById(rate).value;
+    }
 }
         
