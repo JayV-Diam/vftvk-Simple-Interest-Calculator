efadbcd6a5bf515c35532c700049be30c9011774
JVDiamante <jay.vdiamante@gmail.com>
2021-04-08 06:41:16 +0000

    changes made on 4/8/2021, 3pm, w/o highlights

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
+//function that computes the interest 
 function compute()
 {
-    var pincipal = document.getElementById("principal");
-    principal.valueOf();
-    var rate = document.getElementById("rate");
-    rate.valueOf();
-    var year = document.getElementById("years");
-    year.valueOf();
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
     var interest = principal*years*rate / 100;
-    interest.valueOf();
-
-    function SliderValue(slider) {
-        slider = document.getElementById("Slider_Val");
-        alert(slider.valueOf());
+    // if pricipal is either zero or a negative value, show alert message
+    if (principal == 0 || principal <= 0){
+        alert("Enter a Positive Number");
     }
-}
+    // otherwise, display results
+    else{
         
+        document.getElementById("result").innerHTML = ("If you deposit " + Mark(principal) + "\n" 
+        + "at an interest rate of " + rate + "% "
+        + "You will receive an amount of " + interest + " "
+        + "in the year " + "202" + years);
+    } 
+}
+// function that shows the slider value
+function SliderValue() {
+    var slider = document.getElementById("rate").value;
+    document.getElementById("sliderval").innerHTML = slider + "%";
+}
+
+function Mark(){
+    principal.style.backgroundColor = "yellow";
+}
