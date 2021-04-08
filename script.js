696581ff92cc6c2ba65800a1a09539adf0b37569
JVDiamante <jay.vdiamante@gmail.com>
2021-04-08 10:34:23 +0000

    Final Project(04/08/2021)

--- script.js
+++ script.js
@@ -8,22 +8,23 @@ function compute()
     // if pricipal is either zero or a negative value, show alert message
     if (principal == 0 || principal <= 0){
         alert("Enter a Positive Number");
+        location.reload();
     }
     // otherwise, display results
     else{
-        
-        document.getElementById("result").innerHTML = ("If you deposit " + Mark(principal) + "\n" 
-        + "at an interest rate of " + rate + "% "
-        + "You will receive an amount of " + interest + " "
-        + "in the year " + "202" + years);
-    } 
+        document.getElementById("result1").innerHTML = ("If you deposit ");
+        document.getElementById("result1_1").innerHTML = (principal);
+        document.getElementById("result2").innerHTML = ("at an interest rate of ");
+        document.getElementById("result2_2").innerHTML = (rate + "%");
+        document.getElementById("result3").innerHTML = ("You will receive an amount of " );
+        document.getElementById("result3_3").innerHTML = (interest);
+        document.getElementById("result4").innerHTML = ("in the year ");
+        document.getElementById("result4_4").innerHTML = (2021 + parseInt(years));
+    }
+    //principal.style.backgroundColor = "yellow"; 
 }
 // function that shows the slider value
 function SliderValue() {
     var slider = document.getElementById("rate").value;
     document.getElementById("sliderval").innerHTML = slider + "%";
-}
-
-function Mark(){
-    principal.style.backgroundColor = "yellow";
 }
